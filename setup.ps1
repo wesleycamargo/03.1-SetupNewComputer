d2ba006d19f131ad4eda4e1359286edb1c841a00
Wesley Camargo <wesley.camargo@esx.com.br>
2019-03-15 09:07:40 -0300

    commit inicial

---
+++ setup.ps1
@@ -0,0 +1,34 @@
+Set-ExecutionPolicy UnRestrict
+iwr https://chocolatey.org/install.ps1 -UseBasicParsing | iex
+
+
+choco install googlechrome -y
+choco install git -y
+choco install gitextensions -y
+choco install 7zip -y
+choco install vlc -y
+choco install fiddler4 -y
+choco install visualstudiocode -y
+choco install nodejs.install -y
+choco install docker -y
+choco install jre8 -y
+choco install adobereader -y
+choco install skype -y
+choco install filezilla -y
+choco install conemu -y
+choco install microsoft-teams -y
+choco install poshgit -y
+choco install whatsapp -y
+choco install spotify -y
+chocp install lightshot -y
+
+
+dism /online /get-features | more 
+ 
+Dism /online /enable-feature /featurename:NetFx3 /All /Source:D:\sources\sxs /LimitAccess
+Dism /online /enable-feature /featurename:IIS-ASPNET /All /Source:D:\sources\sxs /LimitAccess
+Dism /online /enable-feature /featurename:IIS-ASPNET45 /All /Source:D:\sources\sxs /LimitAccess
+Dism /online /enable-feature /featurename:IIS-BasicAuthentication /All /Source:D:\sources\sxs /LimitAccess
+Dism /online /enable-feature /featurename:IIS-WindowsAuthentication /All /Source:D:\sources\sxs /LimitAccess
+Dism /online /enable-feature /featurename:Containers /All /Source:D:\sources\sxs /LimitAccess
+Dism /online /enable-feature /featurename:Microsoft-Hyper-V-All /All /Source:D:\sources\sxs /LimitAccess
