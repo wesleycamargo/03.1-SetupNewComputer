c0b4338098929b038b567d13824286c91dedf0f8
Wesley Camargo <camargo.wes@gmail.com>
2020-03-26 12:50:48 -0300

    update choco install

--- setup.ps1
+++ setup.ps1
@@ -1,6 +1,4 @@
-Set-ExecutionPolicy UnRestrict
-iwr https://chocolatey.org/install.ps1 -UseBasicParsing | iex
-
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
 #Dev Tools
 choco install git -y
